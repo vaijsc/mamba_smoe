4b62b44ecbfd055a20cbe2155a1bba2d791954d4
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-11-25 05:11:53 +0000

    update config

--- scripts/smoe-m.sh
+++ scripts/smoe-m.sh
@@ -1,7 +1,27 @@
-mkdir -p /path/to/checkpoint/directory/
+#!/bin/bash
+#SBATCH --job-name=smoe_m_clean
+#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/smoe_m_clean.txt
+#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/smoe_m_clean_err.txt
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-018
+#SBATCH --mem-per-gpu=50G
+#SBATCH --cpus-per-gpu=24
+#SBATCH --partition=research
+#SBATCH --mail-type=all
+#SBATCH --mail-user=v.AnhND81@vinai.io
+
+eval "$(conda shell.bash hook)"
+conda activate moe
+cd /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE
+echo "Current path is $PATH"
+echo "Running"
+# nvidia-smi
+echo $CUDA_VISIBLE_DEVICES
+# mkdir -p /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints
 
 args="
---data /path/to/data/directory/wikitext-103/ \
+--data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/ \
 --base_arch transformer \
 --architecture sgsgsgsgsgsg \
 --gate_name smoe \
@@ -21,11 +41,13 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /path/to/checkpoint/directory/smoe.pt \
+--checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/smoe_m_clean.pt \
 "
 
 echo "Training ..."
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 10013 --nproc_per_node=4 --use_env train.py $args
+# CUDA_VISIBLE_DEVICES='0,1,2,3' 
+python -m torch.distributed.launch --master_port 10013 --nproc_per_node=1 --use_env train.py $args
 
 echo "Evaluation ..."
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 10013 --nproc_per_node=4 --use_env train.py $args --resume --full-eval-mode
+# CUDA_VISIBLE_DEVICES='0,1,2,3' 
+python -m torch.distributed.launch --master_port 10013 --nproc_per_node=1 --use_env train.py $args --resume --full-eval-mode
