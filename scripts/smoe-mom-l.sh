67f99af0a4bfa179f099dd9f7accf3de1545a7a9
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-11-26 00:25:54 +0000

    update

--- scripts/smoe-mom-l.sh
+++ scripts/smoe-mom-l.sh
@@ -1,7 +1,27 @@
-mkdir -p /path/to/checkpoint/directory/
+#!/bin/bash
+#SBATCH --job-name=1csmoe_l
+#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/1csmoe_l_err.txt
+#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/1csmoe_l.txt
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-016
+#SBATCH --mem-per-gpu=50G
+#SBATCH --cpus-per-gpu=24
+#SBATCH --partition=research
+#SBATCH --mail-type=all
+#SBATCH --mail-user=v.AnhND81@vinai.io
+
+eval "$(conda shell.bash hook)"
+conda activate moe
+cd /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE
+echo "Current path is $PATH"
+echo "Running"
+# nvidia-smi
+echo $CUDA_VISIBLE_DEVICES
+# mkdir -p /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints
 
 args="
---data /path/to/data/directory/wikitext-103/ \
+--data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/ \
 --base_arch transformer \
 --architecture smsmsmsmsmsmsmsmsmsmsmsm \
 --gate_name smoe \
@@ -17,7 +37,7 @@ args="
 --lr 0.0007 \
 --lr-warmup 5000 \
 --niter 80 \
---batch-sz 24 \
+--batch-sz 12 \
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
@@ -26,11 +46,14 @@ args="
 --mu 0.7 \
 --beta1 0.9 \
 --beta2 0.999 \
---checkpoint /path/to/checkpoint/directory/smoe.pt \
+--checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/1csmoe_l.pt \
 "
 
+# bs 24 -> 12
 echo "Training ..."
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 10013 --nproc_per_node=4 --use_env train.py $args
+# CUDA_VISIBLE_DEVICES='0,1,2,3' 
+python -m torch.distributed.launch --master_port 10013 --nproc_per_node=1 --use_env train.py $args
 
 echo "Evaluation ..."
-CUDA_VISIBLE_DEVICES='0,1,2,3' python -m torch.distributed.launch --master_port 10013 --nproc_per_node=4 --use_env train.py $args --resume --full-eval-mode
+# CUDA_VISIBLE_DEVICES='0,1,2,3' 
+python -m torch.distributed.launch --master_port 10013 --nproc_per_node=1 --use_env train.py $args --resume --full-eval-mode
