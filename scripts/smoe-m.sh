1193e3274c6ccff9d7db1e65fc1acdb503fa1b38
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-11-27 13:06:33 +0000

    update

--- scripts/smoe-m.sh
+++ scripts/smoe-m.sh
@@ -6,7 +6,7 @@
 #SBATCH --gpus-per-node=2
 #SBATCH --nodelist=sdc2-hpc-dgx-a100-016
 #SBATCH --mem-per-gpu=50G
-#SBATCH --cpus-per-gpu=23
+#SBATCH --cpus-per-gpu=4
 #SBATCH --partition=research
 #SBATCH --mail-type=all
 #SBATCH --mail-user=v.AnhND81@vinai.io
