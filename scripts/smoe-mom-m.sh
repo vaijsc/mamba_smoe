23fcc72dcbfa2e36b4583444c06138443331258d
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-11-30 03:41:45 +0000

    update

--- scripts/smoe-mom-m.sh
+++ scripts/smoe-mom-m.sh
@@ -46,7 +46,7 @@ args="
 --mu 0.7 \
 --beta1 0.9 \
 --beta2 0.999 \
---checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_mom.pt \
+--checkpoint /home/anhnd81/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_mom.pt \
 "
 
 echo "Training ..."
