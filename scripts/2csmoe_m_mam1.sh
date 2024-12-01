c2545c5450f558109ae38b97432efd278c0cb7df
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-01 18:20:22 +0000

    update

--- scripts/2csmoe_m_mam1.sh
+++ scripts/2csmoe_m_mam1.sh
@@ -40,7 +40,7 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /home/phinh2/phinh2/workspace/MomentumSMoE/result/checkpoints/2csmoe_m_mam1.pt \
+--checkpoint /lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE/result/checkpoints/2csmoe_m_mam1.pt \
 "
  
 # bs 48 -> 16 -> 32
