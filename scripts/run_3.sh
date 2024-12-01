3326fcaf15135102bb21cf82fd23e332fa75f5e3
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-01 00:42:24 +0000

    update

---
+++ scripts/run_3.sh
@@ -0,0 +1,49 @@
+export CUDA_VISIBLE_DEVICES=0
+export TORCH_DISTRIBUTED_DEBUG=DETAIL
+
+eval "$(conda shell.bash hook)"
+conda activate moe
+# echo "Current path is $PATH"
+echo "Running"
+# nvidia-smi
+echo "Using GPUs: $CUDA_VISIBLE_DEVICES"
+
+args="
+--data /home/ubuntu/workspace/dataset/wikitext-103 \
+--base_arch transformer \
+--architecture smsmsmsmsmsm \
+--gate_name smoe \
+--nlayers 6 \
+--hid-sz 352 \
+--inner-hid-sz 352 \
+--nheads 8 \
+--block-sz 512 \
+--attn-span 1024 \
+--dropout 0.1 \
+--load_balance 0.01 \
+--optim adam \
+--lr 0.0007 \
+--lr-warmup 4000 \
+--niter 80 \
+--batch-sz 16 \
+--batch-split 2 \
+--nbatches 1000 \
+--distributed \
+--gamma1 1.0 \
+--gamma2 1.0 \
+--mu 0.7 \
+--beta1 0.9 \
+--beta2 0.999 \
+--checkpoint /home/ubuntu/workspace/MomentumSMoE/result/checkpoints/mamba_smoe_7.pt \
+"
+
+#--checkpoint /path/to/checkpoint/directory/smoe.pt \
+#block_sz: shape of input
+echo "Training ..."
+#CUDA_VISIBLE_DEVICES='0,1,2,3'
+python -m torch.distributed.launch --master_port 10011 --nproc_per_node=1 --use_env train_1.py $args #\
+# > >(tee -a /home/ubuntu/workspace/MomentumSMoE/result/smoe_s.txt) 2>&1
+
+echo "Evaluation ..."
+python -m torch.distributed.launch --master_port 10011 --nproc_per_node=1 --use_env train_1.py $args --resume --full-eval-mode #\ 
+# > >(tee -a /home/ubuntu/workspace/MomentumSMoE/result/smoe_s.txt) 2>&1
--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import time
 
 from config import PARAMS_CONFIG
 from data import get_train_val_test_data
-from models_1 import TransformerSeq
+from models import TransformerSeq
 from trainer import train_iteration, full_eval
 import datetime
 import wandb
