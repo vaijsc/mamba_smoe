21e79f0a7638fbd4f0788dbdf77cd1218dff5b53
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-02 16:49:19 +0000

    update

--- scripts/2asmoe_mf_mambell.sh
+++ scripts/2asmoe_mf_mambell.sh
@@ -40,7 +40,7 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /home/phinh2/phinh2/workspace/MomentumSMoE/result/checkpoints/2asmoe_mf_mambell.pt \
+--checkpoint /lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE/result/checkpoints/2asmoe_mf_mambell.pt \
 "
  
 # bs 48 -> 16 -> 32
