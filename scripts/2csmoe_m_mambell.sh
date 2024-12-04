34569722f5a6a9b42a02c6d64af3bb49628cd554
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-04 18:03:54 +0000

    update

--- scripts/2csmoe_m_mambell.sh
+++ scripts/2csmoe_m_mambell.sh
@@ -4,9 +4,9 @@
 #SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_m_mambell1.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-015
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-016
 #SBATCH --mem-per-gpu=50G
-#SBATCH --cpus-per-gpu=24
+#SBATCH --cpus-per-gpu=18
 #SBATCH --partition=research
 #SBATCH --mail-type=all
 #SBATCH --mail-user=v.AnhND81@vinai.io
