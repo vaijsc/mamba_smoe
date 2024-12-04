1f218fae8f0ceb717753f410a0c170ac59b3a4fa
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-04 05:04:25 +0000

    update

--- scripts/2asmoe_mf_mamcs.sh
+++ scripts/2asmoe_mf_mamcs.sh
@@ -40,7 +40,7 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE/result/checkpoints/2asmoe_mfcs.pt \
+--checkpoint /home/phinh2/phinh2/workspace/MomentumSMoE/result/checkpoints/2asmoe_mfcs.pt \
 "
  
 # bs 48 -> 16 -> 32
