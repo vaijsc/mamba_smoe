1ae0df6c37e4dcbec53599eacfd47f8f33eec0c2
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-06 01:53:15 +0000

    update

--- scripts/smoe-mom-m.sh
+++ scripts/smoe-mom-m.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=2csmoe_mom1
-#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_mom1_err.txt
-#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_mom1.txt
+#SBATCH --job-name=2csmom
+#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_mom2_err.txt
+#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_mom2.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-015
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-020
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=24
 #SBATCH --partition=research
@@ -21,7 +21,7 @@ echo $CUDA_VISIBLE_DEVICES
 
 
 args="
---data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/ \
+--data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext/ \
 --base_arch transformer \
 --architecture smsmsmsmsmsm \
 --gate_name smoe \
@@ -46,13 +46,13 @@ args="
 --mu 0.7 \
 --beta1 0.9 \
 --beta2 0.999 \
---checkpoint /home/anhnd81/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_mom.pt \
+--checkpoint /home/anhnd81/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_mom2.pt \
 "
 
 echo "Training ..."
 # CUDA_VISIBLE_DEVICES='0,1,2,3' 
-python -m torch.distributed.launch --master_port 10016 --nproc_per_node=2 --use_env train.py $args
+python -m torch.distributed.launch --master_port 10016 --nproc_per_node=2 --use_env train_1.py $args
 
 echo "Evaluation ..."
 # CUDA_VISIBLE_DEVICES='0,1,2,3' 
-python -m torch.distributed.launch --master_port 10016 --nproc_per_node=2 --use_env train.py $args --resume --full-eval-mode
+python -m torch.distributed.launch --master_port 10016 --nproc_per_node=2 --use_env train_1.py $args --resume --full-eval-mode
