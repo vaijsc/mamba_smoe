f46486cfa9084e39cd7d5e04fdde3b6c21de8164
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-09 08:40:25 +0000

    update

--- custom_layers_8.py
+++ custom_layers_8.py
@@ -377,11 +377,11 @@ class FMoE(nn.Module):
         # Use the lower triangular part of the similarity matrix
         similarity_matrix = torch.tril(similarity_matrix)
         # Step 3: Normalize similarities using softmax
-        normalized_similarity = F.softmax(similarity_matrix, dim=-1)  # [batch_size, seq_length, seq_length]
+        # normalized_similarity = F.softmax(similarity_matrix, dim=-1)  # [batch_size, seq_length, seq_length]
         # Update moe_outp using the similarity matrix
         # del similarity_matrix
         # torch.cuda.empty_cache()
-        moe_outp = torch.matmul(normalized_similarity, moe_outp)  # Out-of-place update
+        moe_outp = torch.matmul(similarity_matrix, moe_outp)  # Out-of-place update
 
         # Reshape moe_outp back to the original shape
         moe_outp = moe_outp.view(-1, moe_outp.size(2))
