73b3d98aeaa107a92cefe93e0b7d016bcb508277
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-09 09:15:36 +0000

    update

--- scripts/44_2csmoe_f.sh
+++ scripts/44_2csmoe_f.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=2cfull
-#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_f_err.txt
-#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_f.txt
+#SBATCH --job-name=44f
+#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/44_2csmoe_f_err.txt
+#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/44_2csmoe_f.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-015
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-016
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=24
 #SBATCH --partition=research
@@ -40,7 +40,7 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_f.pt \
+--checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/44_2csmoe_f.pt \
 "
  
 # bs 48 -> 16 -> 32
