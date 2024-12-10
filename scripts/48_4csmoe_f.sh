2389d80944cc9a541dfad930ff52e6c78fab059b
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-10 00:11:35 +0000

    update

--- scripts/48_4csmoe_f.sh
+++ scripts/48_4csmoe_f.sh
@@ -3,8 +3,8 @@
 #SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/44_2csmoe_f_err.txt
 #SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/44_2csmoe_f.txt
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-016
+#SBATCH --gpus-per-node=4
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-015
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=24
 #SBATCH --partition=research
@@ -36,7 +36,7 @@ args="
 --lr 0.0007 \
 --lr-warmup 4000 \
 --niter 80 \
---batch-sz 44 \
+--batch-sz 48 \
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
