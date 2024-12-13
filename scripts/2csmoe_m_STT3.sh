5f1060717123994cf0595067fe797fdd0a6d0821
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-13 23:40:20 +0000

    update

--- scripts/2csmoe_m_STT3.sh
+++ scripts/2csmoe_m_STT3.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodelist=sdc2-hpc-dgx-a100-015
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=24
-#SBATCH --partition=applied
+#SBATCH --partition=research
 #SBATCH --mail-type=all
 #SBATCH --mail-user=v.AnhND81@vinai.io
 
