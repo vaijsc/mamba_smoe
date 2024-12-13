9d09177f1e595de0e6d14b4abd0f4575155b07ac
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-13 23:33:54 +0000

    update

--- scripts/2csmoe_m_STT3.sh
+++ scripts/2csmoe_m_STT3.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 #SBATCH --job-name=STT3
-#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_STT3_err.txt
-#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_STT3.txt
+#SBATCH --output=/lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE/result/2csmoe_STT3_err.txt
+#SBATCH --error=/lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE/result/2csmoe_STT3.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-011
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-015
 #SBATCH --mem-per-gpu=50G
-#SBATCH --cpus-per-gpu=40
+#SBATCH --cpus-per-gpu=24
 #SBATCH --partition=applied
 #SBATCH --mail-type=all
 #SBATCH --mail-user=v.AnhND81@vinai.io
 
 eval "$(conda shell.bash hook)"
 conda activate moe
-cd /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE
+cd /lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE
 echo "Current path is $PATH"
 echo "Running"
 # nvidia-smi
 echo $CUDA_VISIBLE_DEVICES
 
 args="
---data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext/ \
+--data /lustre/scratch/client/vinai/users/phinh2/workspace/dataset/wikitext \
 --base_arch transformer \
 --architecture sgsgsgsgsgsg \
 --gate_name smoe \
@@ -40,7 +40,7 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /home/anhnd81/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_STT3.pt \
+--checkpoint /lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE/result/checkpoints/2csmoe_STT3.pt \
 "
  
 # bs 48 -> 16 -> 32
