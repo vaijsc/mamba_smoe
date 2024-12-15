fea6bae8d8c563506df0be909d4c615118188017
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-15 10:48:21 +0000

    update

--- custom_layers_STT4.py
+++ custom_layers_STT4.py
@@ -360,17 +360,10 @@ class FMoE(nn.Module):
         
         # Compute the norm for each token (along the last dimension, i.e., features)
         norms = torch.norm(moe_inp, p=2, dim=-1, keepdim=True)  # Shape: [batch_size, seq_length, 1]
-
         # Normalize the tokens so that each token has a norm of 1
-        # moe_inp = moe_inp / norms   # Shape: [batch_size, seq_length, dimension]
-        # moe_inp = moe_inp * (1/3)        
+        moe_inp = moe_inp / norms   # Shape: [batch_size, seq_length, dimension]
+        moe_inp = moe_inp * (1/3)        
         
-        # Normalize the tokens in-place to prevent extra memory allocation
-        moe_inp.div_(norms)  # Normalize in-place
-
-        # Scale moe_inp in-place (to avoid creating a new tensor)
-        moe_inp.mul_(1/3)
-
         similarity_matrix = torch.matmul(moe_inp, moe_outp.transpose(1, 2))  # [batch_size, seq_length, seq_length]
         # Use the lower triangular part of the similarity matrix
         similarity_matrix = torch.tril(similarity_matrix)
