20c30d1be9ea66a9a854b093166f287decc4635a
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-15 11:01:05 +0000

    u

--- custom_layers_STT4.py
+++ custom_layers_STT4.py
@@ -355,7 +355,7 @@ class FMoE(nn.Module):
         batch_size = moe_inp.size(0) // seq_length
         # breakpoint()
         # Reshape moe_inp and moe_outp
-        moe_inp = moe_inp.view(batch_size, seq_length, moe_inp.size(1)) # [8, 256, 128]
+        moe_inp = moe_inp.view(batch_size, seq_length, moe_inp.size(1)) 
         moe_outp = moe_outp.view(batch_size, seq_length, moe_outp.size(1))
         
         norms = torch.norm(moe_inp, p=2, dim=-1, keepdim=True)  # Shape: [batch_size, seq_length, 1]
