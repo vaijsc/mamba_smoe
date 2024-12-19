68caf9b9cbbd59c63eee71b41b7890f1012e901e
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-19 03:07:38 +0000

    update

--- scripts/2csmoe_m_STT8.sh
+++ scripts/2csmoe_m_STT8.sh
@@ -4,10 +4,10 @@
 #SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_STT8.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-018-020
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-007
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=24
-#SBATCH --partition=research
+#SBATCH --partition=applied
 #SBATCH --mail-type=all
 #SBATCH --mail-user=v.AnhND81@vinai.io
 
