b1a08b360f96f5547a183197bdbd0ced2f877281
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-20 02:29:40 +0000

    update

--- scripts/2csmoe_m.sh
+++ scripts/2csmoe_m.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=bsl
-#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_bsl_err.txt
-#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_bsl.txt
+#SBATCH --job-name=lr1
+#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_lr1_err.txt
+#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_lr1.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-015
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-013
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=24
 #SBATCH --partition=research
@@ -33,16 +33,17 @@ args="
 --dropout 0.1 \
 --load_balance 0.01 \
 --optim adam \
---lr 0.0007 \
+--lr 0.00007 \
 --lr-warmup 4000 \
 --niter 80 \
 --batch-sz 48 \
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_bsl.pt \
+--checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_lr1.pt \
 "
  
+# 0.0007
 # bs 48 -> 16 -> 32
 echo "Training ..."
 # CUDA_VISIBLE_DEVICES='0,1' 
