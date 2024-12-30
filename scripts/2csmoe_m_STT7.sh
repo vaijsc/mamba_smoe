e5ecd7f3730f029de244bab8fb6f168792644122
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-30 16:02:11 +0000

    u

--- scripts/2csmoe_m_STT7.sh
+++ scripts/2csmoe_m_STT7.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=STT7
-#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_STT7_err.txt
-#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_STT7.txt
+#SBATCH --job-name=S7_1
+#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_STT7_1_err.txt
+#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_STT7_1.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-007
+#SBATCH --nodelist=sdc2-hpc-dgx-a100-016
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=24
-#SBATCH --partition=applied
+#SBATCH --partition=research
 #SBATCH --mail-type=all
 #SBATCH --mail-user=v.AnhND81@vinai.io
 
@@ -20,7 +20,7 @@ echo "Running"
 echo $CUDA_VISIBLE_DEVICES
 
 args="
---data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext/ \
+--data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/ \
 --base_arch transformer \
 --architecture sgsgsgsgsgsg \
 --gate_name smoe \
@@ -40,14 +40,14 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_STT7.pt \
+--checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_STT7_1.pt \
 "
  
 # bs 48 -> 16 -> 32
-# echo "Training ..."
+echo "Training ..."
 # CUDA_VISIBLE_DEVICES='0,1' 
-# python -m torch.distributed.launch --master_port 10006 --nproc_per_node=2 --use_env train_STT7.py $args
+python -m torch.distributed.launch --master_port 10006 --nproc_per_node=2 --use_env train_STT7.py $args
 
 echo "Evaluation ..."
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10006 --nproc_per_node=1 --use_env train_STT7.py $args --resume --full-eval-mode
+python -m torch.distributed.launch --master_port 10006 --nproc_per_node=2 --use_env train_STT7.py $args --resume --full-eval-mode
