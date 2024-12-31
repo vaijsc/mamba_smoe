17373e005702ac5d526aca209b3be24e817ede59
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-31 07:46:54 +0000

    update

--- scripts/2csmoe_mh.sh
+++ scripts/2csmoe_mh.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=r2
-#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_r2_err.txt
-#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_r2.txt
+#SBATCH --job-name=mh
+#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_mh_err.txt
+#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_mh.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
-#SBATCH --nodelist=sdc2-hpc-dgx-a100-010
 #SBATCH --mem-per-gpu=50G
 #SBATCH --cpus-per-gpu=24
 #SBATCH --partition=applied
@@ -40,15 +39,16 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_r2.pt \
+--checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_mh.pt \
 "
  
 # 0.0007
 # bs 48 -> 16 -> 32
 echo "Training ..."
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10008 --nproc_per_node=2 --use_env train_r2.py $args
+python -m torch.distributed.launch --master_port 10011 --nproc_per_node=2 --use_env train_mh.py $args
 
 echo "Evaluation ..."
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10008 --nproc_per_node=2 --use_env train_r2.py $args --resume --full-eval-mode
+python -m torch.distributed.launch --master_port 10011 --nproc_per_node=2 --use_env train_mh.py $args --resume --full-eval-mode
+
