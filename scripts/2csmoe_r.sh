c1be2e41a67fb6c14f36d69dff10c9193bb98783
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-12-31 07:08:20 +0000

    update

--- scripts/2csmoe_r.sh
+++ scripts/2csmoe_r.sh
@@ -68,3 +68,11 @@ python -m torch.distributed.launch --master_port 10009 --nproc_per_node=2 --use_
 echo "Evaluation hidden dim = 32"
 # CUDA_VISIBLE_DEVICES='0,1' 
 python -m torch.distributed.launch --master_port 10009 --nproc_per_node=2 --use_env train_r3.py $args --resume --full-eval-mode
+
+echo "Training hidden dim = 64"
+# CUDA_VISIBLE_DEVICES='0,1' 
+python -m torch.distributed.launch --master_port 10010 --nproc_per_node=2 --use_env train_r4.py $args
+
+echo "Evaluation hidden dim = 64"
+# CUDA_VISIBLE_DEVICES='0,1' 
+python -m torch.distributed.launch --master_port 10010 --nproc_per_node=2 --use_env train_r4.py $args --resume --full-eval-mode
