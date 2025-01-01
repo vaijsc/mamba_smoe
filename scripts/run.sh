60ad22efd35099b675793989ebb70aaff1d50a6c
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2025-01-01 15:34:35 +0000

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -36,7 +36,7 @@ args="
 #block_sz: shape of input
 echo "Training ..."
 #CUDA_VISIBLE_DEVICES='0,1,2,3'
-python -m torch.distributed.launch --master_port 10011 --nproc_per_node=1 --use_env train_STT7.py $args #\
+python -m torch.distributed.launch --master_port 10011 --nproc_per_node=1 --use_env train_r12.py $args #\
 # > >(tee -a /home/ubuntu/workspace/MomentumSMoE/result/smoe_s.txt) 2>&1
 
 # echo "Evaluation ..."
