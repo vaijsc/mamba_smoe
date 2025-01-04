486dca7803ad9050c0a6255c8b6ec0950da56b3a
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2025-01-04 06:36:02 +0000

    u

--- scripts/2bsmoe_r18_19.sh
+++ scripts/2bsmoe_r18_19.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=r18_19
-#SBATCH --output=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_r18_19_err.txt
-#SBATCH --error=/lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/2csmoe_r18_19.txt
+#SBATCH --output=/lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE/result/2csmoe_r18_19_err.txt
+#SBATCH --error=/lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE/result/2csmoe_r18_19.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=2
 #SBATCH --mem-per-gpu=50G
@@ -11,15 +11,17 @@
 #SBATCH --mail-user=v.AnhND81@vinai.io
 
 eval "$(conda shell.bash hook)"
-conda activate /home/anhnd81/.conda/envs/moe
-cd /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE
+# conda activate /home/anhnd81/.conda/envs/moe
+# cd /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE
+conda activate moe
+cd /lustre/scratch/client/vinai/users/phinh2/workspace/MomentumSMoE
 echo "Current path is $PATH"
 echo "Running"
 # nvidia-smi
 echo $CUDA_VISIBLE_DEVICES
 
 args="
---data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/  \
+--data /lustre/scratch/client/vinai/users/phinh2/workspace/dataset/wikitext-103  \
 --base_arch transformer \
 --architecture sgsgsgsgsgsg \
 --gate_name smoe \
@@ -39,21 +41,21 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_r18_19.pt \
+--checkpoint /home/phinh2/phinh2/workspace/MomentumSMoE/result/checkpoints/2csmoe_r18_19.pt \
 "
  
 echo "Training Hierarchical MoE idea, split two subset of experts and each choose top-2, clean data"
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10116 --nproc_per_node=2 --use_env train_r18.py $args
+python -m torch.distributed.launch --master_port 10127 --nproc_per_node=2 --use_env train_r18.py $args
 
 echo "Evaluation Hierarchical MoE idea, split two subset of experts and each choose top-2, clean data"
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10116 --nproc_per_node=2 --use_env train_r18.py $args --resume --full-eval-mode
+python -m torch.distributed.launch --master_port 10127 --nproc_per_node=2 --use_env train_r18.py $args --resume --full-eval-mode
  
 echo "Training Hierarchical MoE idea, split two subset of experts and each choose top-2, attack data"
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10116 --nproc_per_node=2 --use_env train_r19.py $args
+python -m torch.distributed.launch --master_port 10127 --nproc_per_node=2 --use_env train_r19.py $args
 
 echo "Evaluation Hierarchical MoE idea, split two subset of experts and each choose top-2, attack data"
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10116 --nproc_per_node=2 --use_env train_r19.py $args --resume --full-eval-mode
+python -m torch.distributed.launch --master_port 10127 --nproc_per_node=2 --use_env train_r19.py $args --resume --full-eval-mode
