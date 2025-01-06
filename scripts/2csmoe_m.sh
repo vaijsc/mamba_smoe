b90c1d8b24df8d253562b8eea3168a3c3c9eeb5a
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2025-01-06 11:08:17 +0000

    u

--- scripts/2csmoe_m.sh
+++ scripts/2csmoe_m.sh
@@ -86,41 +86,7 @@ echo $CUDA_VISIBLE_DEVICES
 # # CUDA_VISIBLE_DEVICES='0,1' 
 # python -m torch.distributed.launch --master_port 10008 --nproc_per_node=2 --use_env train_ft1.py $args2 --resume --full-eval-mode
 
-# args3="
-# --data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/  \
-# --base_arch transformer \
-# --architecture sgsgsgsgsgsg \
-# --gate_name smoe \
-# --nlayers 6 \
-# --hid-sz 352 \
-# --inner-hid-sz 352 \
-# --nheads 8 \
-# --block-sz 512 \
-# --attn-span 1024 \
-# --dropout 0.1 \
-# --load_balance 0.01 \
-# --optim adam \
-# --lr 0.0007 \
-# --lr-warmup 4000 \
-# --niter 20 \
-# --batch-sz 32 \
-# --batch-split 2 \
-# --nbatches 1000 \
-# --distributed \
-# --checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_bs32_1ft.pt \
-# "
- 
-# # 0.0007
-# # bs 48 -> 16 -> 32
-# echo "Training ..."
-# # CUDA_VISIBLE_DEVICES='0,1' 
-# python -m torch.distributed.launch --master_port 10009 --nproc_per_node=2 --use_env train_1ft.py $args3
-
-# echo "Evaluation ..."
-# # CUDA_VISIBLE_DEVICES='0,1' 
-# python -m torch.distributed.launch --master_port 10009 --nproc_per_node=2 --use_env train_1ft.py $args3 --resume --full-eval-mode
-
-args4="
+args3="
 --data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/  \
 --base_arch transformer \
 --architecture sgsgsgsgsgsg \
@@ -141,15 +107,49 @@ args4="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_bs32_1ft1.pt \
+--checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_bs32_1ft.pt \
 "
  
 # 0.0007
 # bs 48 -> 16 -> 32
 echo "Training ..."
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10011 --nproc_per_node=2 --use_env train_1ft1.py $args4
+python -m torch.distributed.launch --master_port 10009 --nproc_per_node=2 --use_env train_1ft.py $args3
 
 echo "Evaluation ..."
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10011 --nproc_per_node=2 --use_env train_1ft1.py $args4 --resume --full-eval-mode
+python -m torch.distributed.launch --master_port 10009 --nproc_per_node=2 --use_env train_1ft.py $args3 --resume --full-eval-mode
+
+# args4="
+# --data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/  \
+# --base_arch transformer \
+# --architecture sgsgsgsgsgsg \
+# --gate_name smoe \
+# --nlayers 6 \
+# --hid-sz 352 \
+# --inner-hid-sz 352 \
+# --nheads 8 \
+# --block-sz 512 \
+# --attn-span 1024 \
+# --dropout 0.1 \
+# --load_balance 0.01 \
+# --optim adam \
+# --lr 0.0007 \
+# --lr-warmup 4000 \
+# --niter 20 \
+# --batch-sz 32 \
+# --batch-split 2 \
+# --nbatches 1000 \
+# --distributed \
+# --checkpoint /lustre/scratch/client/vinai/users/anhnd81/workspace/MomentumSMoE/result/checkpoints/2csmoe_bs32_1ft1.pt \
+# "
+ 
+# # 0.0007
+# # bs 48 -> 16 -> 32
+# echo "Training ..."
+# # CUDA_VISIBLE_DEVICES='0,1' 
+# python -m torch.distributed.launch --master_port 10011 --nproc_per_node=2 --use_env train_1ft1.py $args4
+
+# echo "Evaluation ..."
+# # CUDA_VISIBLE_DEVICES='0,1' 
+# python -m torch.distributed.launch --master_port 10011 --nproc_per_node=2 --use_env train_1ft1.py $args4 --resume --full-eval-mode
