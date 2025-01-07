78b873b84f126e6915fd8bef27857731d42e645d
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2025-01-07 10:33:26 +0000

    update finetuning

--- scripts/2csmoe_m.sh
+++ scripts/2csmoe_m.sh
@@ -80,13 +80,13 @@ args2="
  
 # 0.0007
 # bs 48 -> 16 -> 32
-echo "Training ..."
-# CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10007 --nproc_per_node=2 --use_env train_ft1.py $args2
+# echo "Training ..."
+# # CUDA_VISIBLE_DEVICES='0,1' 
+# python -m torch.distributed.launch --master_port 10007 --nproc_per_node=2 --use_env train_ft1.py $args2
 
-echo "Evaluation ..."
-# CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10007 --nproc_per_node=2 --use_env train_ft1.py $args2 --resume --full-eval-mode
+# echo "Evaluation ..."
+# # CUDA_VISIBLE_DEVICES='0,1' 
+# python -m torch.distributed.launch --master_port 10007 --nproc_per_node=2 --use_env train_ft1.py $args2 --resume --full-eval-mode
 
 args3="
 --data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/  \
@@ -116,11 +116,11 @@ args3="
 # bs 48 -> 16 -> 32
 echo "Training ..."
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10009 --nproc_per_node=2 --use_env train_1ft.py $args3
+python -m torch.distributed.launch --master_port 10007 --nproc_per_node=2 --use_env train_1ft.py $args3
 
 echo "Evaluation ..."
 # CUDA_VISIBLE_DEVICES='0,1' 
-python -m torch.distributed.launch --master_port 10009 --nproc_per_node=2 --use_env train_1ft.py $args3 --resume --full-eval-mode
+python -m torch.distributed.launch --master_port 10007 --nproc_per_node=2 --use_env train_1ft.py $args3 --resume --full-eval-mode
 
 args4="
 --data /lustre/scratch/client/vinai/users/anhnd81/.cache/wikitext-103/  \
