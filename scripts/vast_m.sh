8398bf4f3f8fce2d688b8abc06b3a5467bcdda67
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2025-01-09 18:07:34 +0000

    update

--- scripts/vast_m.sh
+++ scripts/vast_m.sh
@@ -26,7 +26,7 @@ args="
 "
  
 echo "Training ..."
-CUDA_VISIBLE_DEVICES='0,1' python -m torch.distributed.launch --master_port 10017 --nproc_per_node=2 --use_env train.py $args
+WANDB_API_KEY=99a0a70a15a59905811d9ab32443e1a18cad8b1a CUDA_VISIBLE_DEVICES='0,1' python -m torch.distributed.launch --master_port 10017 --nproc_per_node=2 --use_env train.py $args
 
 echo "Evaluation ..."
-CUDA_VISIBLE_DEVICES='0,1' python -m torch.distributed.launch --master_port 10017 --nproc_per_node=2 --use_env train.py $args --resume --full-eval-mode
+WANDB_API_KEY=99a0a70a15a59905811d9ab32443e1a18cad8b1a CUDA_VISIBLE_DEVICES='0,1' python -m torch.distributed.launch --master_port 10017 --nproc_per_node=2 --use_env train.py $args --resume --full-eval-mode
