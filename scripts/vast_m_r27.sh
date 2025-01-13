b47bf2608358947e1a8e80ceb9ffb72579457d8e
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2025-01-13 02:43:12 +0000

    update

--- scripts/vast_m_r27.sh
+++ scripts/vast_m_r27.sh
@@ -1,3 +1,4 @@
+export CUDA_VISIBLE_DEVICES='0,1'
 args="
 --data /home/anh/wikitext-103  \
 --base_arch transformer \
@@ -26,7 +27,7 @@ args="
 "
 
 echo "Training ..."
-WANDB_API_KEY="99a0a70a15a59905811d9ab32443e1a18cad8b1a" CUDA_VISIBLE_DEVICES='0,1' CUDA_LAUNCH_BLOCKING=1 python -m torch.distributed.launch --master_port 10017 --nproc_per_node=2 --use_env train_r27.py $args
+WANDB_API_KEY="99a0a70a15a59905811d9ab32443e1a18cad8b1a" python -m torch.distributed.launch --master_port 10017 --nproc_per_node=2 --use_env train_r27.py $args
 
 echo "Evaluation ..."
-WANDB_API_KEY="99a0a70a15a59905811d9ab32443e1a18cad8b1a" CUDA_VISIBLE_DEVICES='0,1' CUDA_LAUNCH_BLOCKING=1 python -m torch.distributed.launch --master_port 10017 --nproc_per_node=2 --use_env train_r27.py $args --resume --full-eval-mode
+WANDB_API_KEY="99a0a70a15a59905811d9ab32443e1a18cad8b1a" python -m torch.distributed.launch --master_port 10017 --nproc_per_node=2 --use_env train_r27.py $args --resume --full-eval-mode
