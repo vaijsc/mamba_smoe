1e444dd34811251e6f6373e8037aefb7c191ba75
icml2 <icml2>
2025-01-18 14:20:13 +0000

    update

--- scripts/run_vast.sh
+++ scripts/run_vast.sh
@@ -22,13 +22,13 @@ args="
 --batch-split 2 \
 --nbatches 1000 \
 --distributed \
---checkpoint /home/anh/MomentumSMoE/result/checkpoints/r47_t.pt
+--checkpoint /home/anh/MomentumSMoE/result/checkpoints/r48_t.pt
 "
 
 #--checkpoint /path/to/checkpoint/directory/smoe.pt \
 #block_sz: shape of input
 echo "Training ..."
-CUDA_VISIBLE_DEVICES='7' python -m torch.distributed.launch --master_port 10011 --nproc_per_node=1 --use_env train_r47.py $args #\
+CUDA_VISIBLE_DEVICES='7' python -m torch.distributed.launch --master_port 10011 --nproc_per_node=1 --use_env train_r48.py $args #\
 # > >(tee -a /home/ubuntu/workspace/MomentumSMoE/result/smoe_s.txt) 2>&1
 
 # echo "Evaluation ..."
