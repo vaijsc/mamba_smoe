8d39c4fe4af6ed96cce91f79bcef46d21128db4b
icml2 <icml2>
2025-01-22 04:28:35 +0000

    update

--- custom_layers_r44_mh.py
+++ custom_layers_r44_mh.py
@@ -265,8 +265,8 @@ class FMoE(nn.Module):
         self.mask = mask
         self.mask_dict = mask_dict
         self.moe_group = moe_group
-        self.weights = nn.Linear(self.d_model, self.d_model)
-        # self.weight = nn.Parameter(torch.ones([self.d_model, 1]))
+        # self.weights = nn.Linear(self.d_model, self.d_model)
+        self.weight_out = nn.Parameter(torch.ones([self.d_model, self.d_model]))
         # self.weights = nn.Linear(self.d_model, 1)
         
     def expert_fn(self, inp, fwd_expert_count):
@@ -499,7 +499,8 @@ class FMoE(nn.Module):
         # g1 = torch.sigmoid(torch.matmul(moe_inp, self.weight)).to(moe_outp_1.device)
         # moe_outp = g1 * moe_outp_1 + (1 - g1) * moe_outp_2
         moe_outp = torch.cat([moe_outp_2, moe_outp_1], dim=-1)
-        moe_outp = self.weights(moe_outp)
+        # moe_outp = self.weights(moe_outp)
+        moe_outp = torch.matmul(moe_outp, self.weight_out)
         if self.slice_size > 1:
 
             def all_gather_func(tensor):
