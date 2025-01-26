886902a08e7e657a1efa97503fc6a97bca6666a1
icml2 <icml2>
2025-01-26 07:50:01 +0000

    u

--- scripts/run_vast.sh
+++ scripts/run_vast.sh
@@ -29,7 +29,7 @@ args="
 #block_sz: shape of input
 echo "Training ..."
 # CUDA_VISIBLE_DEVICES='7' 
-python -m torch.distributed.launch --master_port 10011 --nproc_per_node=1 --use_env train_r66.py $args #\
+python -m torch.distributed.launch --master_port 10011 --nproc_per_node=1 --use_env train_r69.py $args #\
 # > >(tee -a /home/ubuntu/workspace/MomentumSMoE/result/smoe_s.txt) 2>&1
 
 # 69909127
