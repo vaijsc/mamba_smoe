ec563f041ddb5d7ef1da0354d7dca7edf9fecf13
icml2 <icml2>
2025-01-26 17:47:04 +0000

    u

--- scripts/vast_m_r68.sh
+++ scripts/vast_m_r68.sh
@@ -29,10 +29,10 @@ args="
 "
 
 echo "Training ..."
-WANDB_API_KEY="99a0a70a15a59905811d9ab32443e1a18cad8b1a" python -m torch.distributed.launch --master_port 10023 --nproc_per_node=2 --use_env train_r68.py $args
+WANDB_API_KEY="99a0a70a15a59905811d9ab32443e1a18cad8b1a" python -m torch.distributed.launch --master_port 10024 --nproc_per_node=2 --use_env train_r68.py $args
 
 echo "Evaluation ..."
-WANDB_API_KEY="99a0a70a15a59905811d9ab32443e1a18cad8b1a" python -m torch.distributed.launch --master_port 10023 --nproc_per_node=2 --use_env train_r68.py $args --resume --full-eval-mode
+WANDB_API_KEY="99a0a70a15a59905811d9ab32443e1a18cad8b1a" python -m torch.distributed.launch --master_port 10024 --nproc_per_node=2 --use_env train_r68.py $args --resume --full-eval-mode
 
 # đã thử lb 
 # ko thử lb (hiện tại) + xavier initialization
